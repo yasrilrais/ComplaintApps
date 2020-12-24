0f5b19d52d99b8de7e6b108119641258c12166f6
Yasril <yasrilrais6@gmail.com>
2020-12-25 03:04:16 +0700

    tes

---
+++ app/src/main/java/com/example/complaintapps/KomplainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.complaintapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KomplainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
