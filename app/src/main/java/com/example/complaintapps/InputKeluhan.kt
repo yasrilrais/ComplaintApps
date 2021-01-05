cf74b37f02e800d5eb2e0e725665c6a205938e9d
rizky <fajarfadilla1@gmail.com>
2021-01-06 01:09:30 +0700

    Halaman keluhan

---
+++ app/src/main/java/com/example/complaintapps/InputKeluhan.kt
@@ -0,0 +1,11 @@
+package com.example.complaintapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputKeluhan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_keluhan)
+    }
+}
